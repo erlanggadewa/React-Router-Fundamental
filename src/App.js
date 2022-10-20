0c16f2dd337c9deb39d1f2a481a50f1d3dcc81c1
erlanggadewa <erlanggadewa@student.telkomuniversity.ac.id>
2022-10-20 08:25:40 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import DetailPageWrapper from './pages/DetailPage';
+import HomePage from './pages/HomePage';
+import NowPlayingPage from './pages/NowPlayingPage';
+import SearchPageWrapper from './pages/SearchPage';
+import UpcomingPage from './pages/UpcomingPage';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Movie Catalogue</h1>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/now-playing" element={<NowPlayingPage />} />
+          <Route path="/upcoming" element={<UpcomingPage />} />
+          <Route path="/search" element={<SearchPageWrapper />} />
+          <Route path="/movies/:id" element={<DetailPageWrapper />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+import MovieList from '../components/MovieList';
+import { searchMovies } from '../utils/data';
+import PropTypes from 'prop-types';
+import { useSearchParams } from 'react-router-dom';
+
+function SearchPageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const changeSearchParams = (keyword) => {
+    setSearchParams({ title: keyword });
+  };
+
+  return <SearchPage onSearch={changeSearchParams} />;
+}
+
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foundedMovies: []
+    };
+
+    this.onSearch = this.onSearch.bind(this);
+  }
+
+  onSearch(keyword) {
+    this.setState(() => {
+      return {
+        foundedMovies: searchMovies(keyword)
+      };
+    });
+
+    this.props.onSearch(keyword);
+  }
+
+  render() {
+    return (
+      <section>
+        <h2>Search Movie</h2>
+        <SearchBar search={this.onSearch} />
+        <MovieList movies={this.state.foundedMovies} />
+      </section>
+    );
+  }
+}
+
+SearchPage.propTypes = {
+  onSearch: PropTypes.func
+};
+
+export default SearchPageWrapper;
